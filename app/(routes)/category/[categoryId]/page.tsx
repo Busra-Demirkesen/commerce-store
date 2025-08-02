7daf9829f285b8c5107d5f0f9a9eba78cb248e33
Busra-Demirkesen <busrademirkesen2@gmail.com>
2025-08-02 21:20:15 +0200

    feat: add MobileFilters component to category page; integrate size and color filters for mobile view

--- app/(routes)/category/[categoryId]/page.tsx
+++ app/(routes)/category/[categoryId]/page.tsx
@@ -5,6 +5,7 @@ import getSizes from "@/actions/get-sizes";
 import { Billboard } from "@/components/billboard";
 import Container from "@/components/ui/container";
 import Filter from "./components/filter";
+import MobileFilters from "./components/mobile-filters";
 import NoResults from "@/components/ui/no-results";
 import ProductCard from "@/components/ui/product-card";
 
@@ -43,6 +44,10 @@ const CategoryPage: React.FC<CategoryPageProps> = async ({
                 )}
                 <div className="px-4 sm:px-6 lg:px-8 pb-24">
                     <div className="lg:grid lg:grid-cols-5 lg:gap-x-8">
+                        <MobileFilters
+                          sizes={sizes}
+                          colors={colors}
+                         />
                         <div className="hidden lg:block">
                             <Filter
                                 valueKey="sizeId"
