df9a7733e2ad78c6c5ec2b7a0ecbba5dc8bffac3
Busra-Demirkesen <busrademirkesen2@gmail.com>
2025-08-02 22:17:56 +0200

    fix: clean up formatting and improve code consistency in ProductCard component

--- components/ui/product-card.tsx
+++ components/ui/product-card.tsx
@@ -1,9 +1,9 @@
-'use client';
+"use client";
 
 import { Product } from "@/types";
 import Image from "next/image";
 import IconButton from "@/components/ui/icon-button";
-import {  Expand, ShoppingCart } from "lucide-react";
+import { Expand, ShoppingCart } from "lucide-react";
 import { FC, MouseEventHandler } from "react";
 import Currency from "@/components/ui/currency";
 import { useRouter } from "next/navigation";
@@ -14,20 +14,22 @@ interface ProductCardProps {
 }
 
 const ProductCard: FC<ProductCardProps> = ({ data }) => {
-
   const previewModal = usePreviewModal();
   const router = useRouter();
-  const handleClick = () =>{
+  const handleClick = () => {
     router.push(`/product/${data?.id}`);
-  }
+  };
 
-const onPreview:MouseEventHandler<HTMLButtonElement> = (event) =>{
- event.stopPropagation();
- previewModal.onOpen(data);
-}
+  const onPreview: MouseEventHandler<HTMLButtonElement> = (event) => {
+    event.stopPropagation();
+    previewModal.onOpen(data);
+  };
 
   return (
-    <div className="bg-white group cursor-pointer rounded-xl border p-3 space-y-4" onClick={handleClick}>
+    <div
+      className="bg-white group cursor-pointer rounded-xl border p-3 space-y-4"
+      onClick={handleClick}
+    >
       <div className="relative aspect-square rounded-xl bg-gray-100">
         <Image
           alt="Product"
@@ -39,9 +41,10 @@ const onPreview:MouseEventHandler<HTMLButtonElement> = (event) =>{
         <div className="opacity-0 group-hover:opacity-100 transition absolute w-full px-6 bottom-5 z-10">
           <div className="flex gap-x-6 justify-center">
             <IconButton
-              onClick={(onPreview) => {}}
+              onClick={onPreview}
               icon={<Expand size={20} className="text-gray-600" />}
             />
+
             <IconButton
               onClick={() => {}}
               icon={<ShoppingCart size={20} className="text-gray-600" />}
@@ -51,15 +54,11 @@ const onPreview:MouseEventHandler<HTMLButtonElement> = (event) =>{
       </div>
 
       <div>
-        <p className="font-semibold text-lg">
-          {data.name}
-        </p>
-        <p className="text-sm text-gray-500">
-          {data.category.name}
-        </p>
+        <p className="font-semibold text-lg">{data.name}</p>
+        <p className="text-sm text-gray-500">{data.category.name}</p>
       </div>
       <div className="flex items-center justify-between">
-        <Currency value={data?.price}/>
+        <Currency value={data?.price} />
       </div>
     </div>
   );
