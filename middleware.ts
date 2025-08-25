5821b09ecc51268fd5b863c52440640c08eb3152
Busra-Demirkesen <busrademirkesen2@gmail.com>
2025-08-25 16:20:23 +0200

    middleware düzeltildi

--- middleware.ts
+++ middleware.ts
@@ -4,9 +4,7 @@ export default clerkMiddleware(); // Call clerkMiddleware directly without optio
 
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
-    '/(api|trpc)(.*)',
+    // Match all request paths except for files with extensions, Next.js internal paths, and Clerk's auth routes
+    '/((?!_next|[^?]*\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)|(api|trpc)(.*)|(sign-in|sign-up)(.*))).*)',
   ],
 };
