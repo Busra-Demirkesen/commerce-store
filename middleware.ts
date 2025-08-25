48978a9b5bb343d2cf4f11446184e55a430c95b0
Busra-Demirkesen <busrademirkesen2@gmail.com>
2025-08-25 15:50:25 +0200

    refactor: replace authMiddleware with clerkMiddleware and update matcher configuration for improved route handling

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,12 @@
-import { authMiddleware } from "@clerk/nextjs";
+import { clerkMiddleware } from '@clerk/nextjs/server'; // Use clerkMiddleware
 
-export default authMiddleware({
-  publicRoutes: ['/', '/product/(.*)', '/category/(.*)', '/api/(.*)'],
-});
+export default clerkMiddleware(); // Call clerkMiddleware directly
 
 export const config = {
-  matcher: ['/((?!.+\.[\w]+$|_next).*)', '/(api|trpc)(.*)'],
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    '/((?!_next|[^?]*\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
+  ],
 };
