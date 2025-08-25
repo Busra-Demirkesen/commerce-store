a3cdad63475e666eaa70f1deebc954efd2f1b1ee
Busra-Demirkesen <busrademirkesen2@gmail.com>
2025-08-25 16:23:46 +0200

    middleware deploy hatası çözümü

--- middleware.ts
+++ middleware.ts
@@ -4,7 +4,8 @@ export default clerkMiddleware(); // Call clerkMiddleware directly without optio
 
 export const config = {
   matcher: [
-    // Match all request paths except for files with extensions, Next.js internal paths, and Clerk's auth routes
-    '/((?!_next|[^?]*\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)|(api|trpc)(.*)|(sign-in|sign-up)(.*))).*)',
+    '/((?!_next|[^?]*\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    '/api/(.*)',
+    '/trpc/(.*)',
   ],
 };
