2284ee374a6205a810a5f70d6cf5b1557f328302
Busra-Demirkesen <busrademirkesen2@gmail.com>
2025-09-29 09:53:56 +0200

    refactor: wrap application layout with SearchProvider to enable search functionality integration

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,7 @@ import Navbar from "@/components/navbar"; // Yorum satırı kaldırıldı
 import ModalProvider from "@/providers/modal-provider"; // Yorum satırı kaldırıldı
 import ToastProvider from "@/providers/toast-provider"; // Yorum satırı kaldırıldı
 import { ClerkProvider } from '@clerk/nextjs';
+import { SearchProvider } from '@/providers/search-modal-provider'; // Yorum satırı kaldırıldı
 
 const font = Urbanist({
   subsets: ["latin"],
@@ -25,7 +26,7 @@ export default function RootLayout({
 }) {
   return (
     <ClerkProvider>
-      {/* <SearchProvider> */}
+      <SearchProvider> {/* Wrap with SearchProvider */}
         <html lang="en">
           <body className={font.className}>
             <ModalProvider/>
@@ -35,7 +36,7 @@ export default function RootLayout({
             <Footer />
           </body>
         </html>
-      {/* </SearchProvider> */}
+      </SearchProvider>
     </ClerkProvider>
   );
 }
